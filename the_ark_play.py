444078f89564a271b8dbf9aca59c75f59d23cd6a
Gabrielle Blake <gaby.blake2@gmail.com>
2021-05-07 20:49:10 -0400

    refactored code to update View class

--- the_ark_play.py
+++ the_ark_play.py
@@ -3,8 +3,8 @@ import pygame, sys
 from pygame.locals import *
 import random, time
 
-from the_ark_view import *
 from the_ark_game import *
+from the_ark_view import *
 from the_ark_controller import *
 
 def main():
@@ -17,9 +17,6 @@ def main():
     game = ArkGame()
     game_view = ArkView(game)
     
-    #Setting up Fonts
-    font = pygame.font.SysFont("Verdana", 60)
-    font_small = pygame.font.SysFont("Verdana", 20)
     game_over = font.render("Game Over", True, game.BLACK)
     game_won = font.render("You won!", True, game.BLACK)
     
@@ -41,10 +38,7 @@ def main():
     
     
     
-    #this block of code can be a View class method that starts the screen
-    DISPLAYSURF = pygame.display.set_mode((game.SCREEN_WIDTH, game.SCREEN_HEIGHT))
-    DISPLAYSURF.fill(game.WHITE)
-    pygame.display.set_caption("Game")
+    DISPLAYSURF = game_view.set_screen()
     
     
     while True:     
@@ -53,18 +47,9 @@ def main():
                 pygame.quit()
                 sys.exit()
         
-        # this block of code could be an "update_view" method that paint the screen
-        # white and redraw the player; this is definitely a View method
-        DISPLAYSURF.blit(game.background, (0,0))
-        scores = font_small.render(f"SCORE: {game.score}", True, game.WHITE)
-        DISPLAYSURF.blit(scores, (game.SCREEN_WIDTH-100,10)) 
-        life = font_small.render(f"LIVES: {game.lives}", True, game.WHITE)
-        DISPLAYSURF.blit(life, (game.SCREEN_WIDTH-100,30))
-        
-        for sprite in all_sprites:
-            DISPLAYSURF.blit(sprite.image, sprite.rect)
-            if sprite in movable_sprites:
-                sprite.move()
+        # redraws and updates everything on the screen, including the
+        # background, sprites, and texts
+        game_view.update_screen(DISPLAYSURF, all_sprites, movable_sprites)
         
         if pygame.sprite.spritecollideany(player, seeds):
             item = pygame.sprite.spritecollideany(player, seeds)
--- the_ark_view.py
+++ the_ark_view.py
@@ -2,13 +2,20 @@ import pygame, sys
 from pygame.locals import *
 import random, time
 
+pygame.init()
+
+#Setting up Fonts
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+
+    
+
 
 class ArkView():
     """
     A class that represents the game view of the game,
     The Ark.
     """
-    pygame.init()
     
     def __init__(self, ark_game):
         """
@@ -23,15 +30,50 @@ class ArkView():
         """
         return self._game
     
-    def draw(self):
+    def set_screen(self):
         """
-        Creates the game map that players will see when they play.
+        Initializes the game screen and creates the screen
+        window's surface.
+        
+        Args:
+            This method has no arguments.
+            
+        Returns:
+            The surface that represents the window screen the
+            game.
         """
-        # We will use Pygame to populate this method
-        pass
+        DISPLAYSURF = pygame.display.set_mode((self.game.SCREEN_WIDTH, 
+                                               self.game.SCREEN_HEIGHT))
+        DISPLAYSURF.fill(self.game.WHITE)
+        pygame.display.set_caption("Game")
+        
+        return DISPLAYSURF
     
-
-    
+    def update_screen(self, DISPLAYSURF, all_sprites, movable_sprites):
+        """
+        Updates and redraws all sprites and text on the screen.
+        
+        Args:
+            DISPLAYSURF: The window screen surface of the game
+            all_sprites: The Sprite Group that holds all of the sprites
+            movable_sprites: The Sprite Group that holds all of the
+            sprites that have move methods.
+        
+        Returns:
+        This method does not return anything.
+        """
+        # updates the window screen and the Score and Lives text
+        DISPLAYSURF.blit(self.game.background, (0,0))
+        scores = font_small.render(f"SCORE: {self.game.score}", True, self.game.WHITE)
+        DISPLAYSURF.blit(scores, (self.game.SCREEN_WIDTH-100,10)) 
+        life = font_small.render(f"LIVES: {self.game.lives}", True, self.game.WHITE)
+        DISPLAYSURF.blit(life, (self.game.SCREEN_WIDTH-100,30))
+        
+        # updates the sprites 
+        for sprite in all_sprites:
+            DISPLAYSURF.blit(sprite.image, sprite.rect)
+            if sprite in movable_sprites:
+                sprite.move()
     
     
     
